b2f277531e1ebf92e261b56757a05910fd9b44ea
that-ar-guy <mohdarahman124@gmail.com>
2023-12-13 11:08:32 +0530

    bicep template rendering

--- app.py
+++ app.py
@@ -1,52 +1,52 @@
-from flask import Flask, render_template, request, redirect, url_for, Response
+from flask import Flask, render_template, Response, request, redirect, url_for
 from flask_socketio import SocketIO
+from camera import VideoCamera
 import cv2
-from camera import VideoCamera  # Assuming the camera module is in a file named camera.py
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def landing():
-    if request.method == 'POST':
-        exercise = request.form['exercise']
-        if exercise == 'bicep_curls':
-            return redirect(url_for('bicep_curls_page'))
-        elif exercise == 'squats':
-            return redirect(url_for('squats_page'))
     return render_template('landing.html')
 
-@app.route('/bicep_curls', methods=['GET', 'POST'])
-def bicep_curls_page():
+@app.route('/submit', methods=['POST'])
+def submit():
     if request.method == 'POST':
-        total_curls = int(request.form['total_curls'])
-        return redirect(url_for('video_feed'))
-    return render_template('bicep_curls.html')  # Replace 10 with the default number of curls
-  # Replace 10 with the default number of curls
+        form_type = request.form.get('form_type')
 
-@app.route('/squats', methods=['GET', 'POST'])
-def squats_page():
-    if request.method == 'POST':
-        total_squats = int(request.form['total_squats'])
-        return redirect(url_for('video_feed'))
-    return render_template('squats.html')
+        if form_type == 'bicep_curl':
+            bicep_curls = request.form.get('bicep_curls')
+            return redirect(url_for('bicep_curl', count=bicep_curls))
+        elif form_type == 'squats':
+            squats = request.form.get('squats')
+            return redirect(url_for('squats', count=squats))
+        else:
+            return redirect(url_for('landing'))
 
+@app.route('/bicep_curl/<count>')
+def bicep_curl(count):
+    return render_template('bicep_curls.html', count=count)
 
-def gen(camera):
+@app.route('/squats/<count>')
+def squats(count):
+    return render_template('squats.html', count=count)
+
+def gen(camera, total_curls):
     while True:
-        jpeg = camera.get_frame()
+        jpeg, remaining_curls = camera.get_frame(count)
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
-        socketio.emit('update')
+        socketio.emit('update', {'remaining_curls': remaining_curls})
 
         yield (b'--frame\r\n'
                b'Content-type: image/jpeg\r\n\r\n' + jpeg
                + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace;boundary=frame')
+@app.route('/video_feed/<int:count>')
+def video_feed(count):
+    return Response(gen(VideoCamera(), count), mimetype='multipart/x-mixed-replace;boundary=frame')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
